61b8b2ef66328771c45494b61bc431e3862a00a2
Marina Kosovic <marinakosovic15@gmail.com>
2017-09-25 14:46:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,16 +16,16 @@ function divide(a , b){
 }
 
 function inc(n){
-  return n + 1;
+  return n += 1;
 }
 
 function dec(n){
-  return n-1;
+  return n -= 1;
 }
 
 
-  function makeInt (n, 10){
-    return makeInt.toString(n);
+  function makeInt (n){
+    return parseInt.(n,10 );
 }
 
 function preserveDecimal (n) {
